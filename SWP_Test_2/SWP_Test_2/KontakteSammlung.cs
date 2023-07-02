f885b43cb136e314e74fa7b391e3c9c3db6796a8
noobprogrammer05 <marinkovic.adrian.2005@outlook.com>
2023-07-02 13:25:35 +0200

    2.SWP_Test in C# programiiert

---
+++ SWP_Test_2/SWP_Test_2/KontakteSammlung.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP_Test_2
+{
+    internal class KontakteSammlung
+    {
+        private string[] kontakte;
+
+        public int Vergleichen(string a,string b)
+        {
+            return a.CompareTo(b);
+        }
+        public bool IstVoll()
+        {
+            if (kontakte[kontakte.Length- 1] != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int SuchePosition(string neuerKontakt)
+        {
+            for(int i = 0; i < kontakte.Length; i++)
+            {
+                if (kontakte[i] == null || Vergleichen(kontakte[i], neuerKontakt) > 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Verschieben(int pos)
+        {
+            for(int i = kontakte.Length-2; i >= 0; i--)
+            {
+                kontakte[i+1] = kontakte[i];
+
+            }
+            kontakte[pos] = null;
+        }
+
+        public bool Eintragen(string neuerKontakt)  
+        {
+            if(IstVoll() == true)
+            {
+                return false;
+            }
+            else
+            {
+                int pos = SuchePosition(neuerKontakt);
+                Verschieben(pos);
+                kontakte[pos] = neuerKontakt;
+                return true;
+            }
+
+
+        }
+
+    }
+}
